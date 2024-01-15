b698dec50077eeec8c5d1033bcf3c3a9b608c3de
abhishek-rabidas <avisec07@gmail.com>
2024-01-15 15:25:31 +0530

    controller mapping and post API

--- src/main/java/com/abhishek/webfluxdemo/Record.java
+++ src/main/java/com/abhishek/webfluxdemo/Record.java
@@ -1,18 +1,25 @@
 package com.abhishek.webfluxdemo;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
-
 import java.util.Date;
 
 @Table
 @Getter
 @Setter
+@NoArgsConstructor
 public class Record {
     @Id
     private int id;
     private String details;
     private Date date;
+
+    public Record(RecordRequest request) {
+        this.id = request.getId();
+        this.details = request.getDetails();
+        this.date = new Date();
+    }
 }
---
+++ src/main/java/com/abhishek/webfluxdemo/RecordRequest.java
@@ -0,0 +1,13 @@
+package com.abhishek.webfluxdemo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class RecordRequest {
+    private int id;
+    private String details;
+}
